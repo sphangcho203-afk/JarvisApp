2c04d86c89a8e6d02c4261f7e4c310c3c6555e4f
sphangcho203-afk <sphangcho203@gmail.com>
2026-07-18 03:50:01 +0530

    Update Android instrumentation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,8 +87,8 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
 
-    androidTestImplementation("androidx.test:core-ktx:1.6.1")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test:runner:1.6.2")
-    androidTestImplementation("androidx.test:rules:1.6.1")
+    androidTestImplementation("androidx.test:core-ktx:1.7.0")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test:runner:1.7.0")
+    androidTestImplementation("androidx.test:rules:1.7.0")
 }
