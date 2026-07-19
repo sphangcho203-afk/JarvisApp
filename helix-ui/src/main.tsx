ea99b776efb1172d665cfc2543690b1bc5acd401
sphangcho203-afk <sphangcho203@gmail.com>
2026-07-19 19:23:06 +0530

    Load FRIDAY cinematic OS styles

--- helix-ui/src/main.tsx
+++ helix-ui/src/main.tsx
@@ -5,5 +5,6 @@ import './styles.css'
 import './designLock.css'
 import './polishLock.css'
 import './workspaceRail.css'
+import './cinematicOs.css'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
